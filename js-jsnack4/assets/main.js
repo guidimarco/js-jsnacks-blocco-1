34912cfbb2b62e32c32f81c5403ef175d2dc7adb
Marco Guidi <mark.88g@gmail.com>
2020-10-15 15:04:02 +0200

    realizzato jsnack 4

--- js-jsnack4/assets/main.js
+++ js-jsnack4/assets/main.js
@@ -3,29 +3,25 @@
 var userNumbers = [];
 // control var --> if isNaN restart the cicle!
 var userNumberControl;
-var userNumbersSum;
 
 // ASK FOR A NUMBER
-for (var i = 0; i < 5; i++) {
+for (var i = 0; i < 6; i++) {
     // i iteration - insert a number with prompt
     userNumberControl = parseInt(prompt("Inserisci un numero"));
 
     if (isNaN(userNumberControl)) {
+        // isNaN - repeat the cicle
         alert("Devi inserire un numero!");
         i = i - 1;
+    } else if (userNumberControl % 2 == 0) {
+        // userNumber even
+        console.log("Il numero inserito è pari: " + userNumberControl);
     } else {
+        // userNumber odd
         userNumbers.push(userNumberControl);
+        console.log("Il numero inserito è dispari: " + userNumberControl);
     }
 }
 
 // CHECK
 console.log(userNumbers);
-
-// SUM NUMBERS
-userNumbersSum = 0;
-
-for (var i = 0; i < userNumbers.length; i++) {
-    userNumbersSum = userNumbersSum + userNumbers[i];
-}
-
-console.log(userNumbersSum);
