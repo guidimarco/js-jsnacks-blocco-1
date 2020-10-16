8eb2dde6d579f0caa1281c44481d1a1167034c8c
Marco Guidi <mark.88g@gmail.com>
2020-10-16 12:59:10 +0200

    corretti js 3 e 4

--- js-jsnack3/assets/main.js
+++ js-jsnack3/assets/main.js
@@ -1,33 +1,62 @@
-// ASSEGNAZIONE VARIABILI
-// array with user's numbers
-var userNumbers = [];
+// <1-fold --- VERSION 1.0 --- take the users numbers
+// // VAR ASSIGNMENT
+// // array with user's numbers
+// var userNumbers = [];
+// // control var --> if isNaN restart the cicle!
+// var userNumberControl;
+// var userNumbersSum;
+// // number of request
+// var request = 5;
+//
+// // ASK FOR A NUMBER
+// for (var i = 0; i < request; i++) {
+//     // i iteration - insert a number with prompt
+//     userNumberControl = parseInt(prompt("Inserisci un numero"));
+//
+//     if (isNaN(userNumberControl)) {
+//         alert("Devi inserire un numero!");
+//         request = request + 1;
+//     } else {
+//         userNumbers.push(userNumberControl);
+//     }
+// }
+//
+// // CHECK
+// console.log(userNumbers);
+//
+// // SUM NUMBERS
+// userNumbersSum = 0;
+//
+// for (var i = 0; i < userNumbers.length; i++) {
+//     userNumbersSum = userNumbersSum + userNumbers[i];
+// }
+//
+// console.log(userNumbersSum);
+// </1-fold
+
+// <1-fold --- VERSION 2.0 --- optimized version --> forgot the users numbers
+// VAR ASSIGNMENT
 // control var --> if isNaN restart the cicle!
-var userNumberControl;
-var userNumbersSum;
+var userNumber;
 // number of request
 var request = 5;
 
-// ASK FOR A NUMBER
+var userNumbersSum = 0;
+
+// ASK FOR A NUMBER AND CALCUTE SUM
 for (var i = 0; i < request; i++) {
     // i iteration - insert a number with prompt
-    userNumberControl = parseInt(prompt("Inserisci un numero"));
+    userNumber = parseInt(prompt("Inserisci un numero"));
 
-    if (isNaN(userNumberControl)) {
+    if (isNaN(userNumber)) {
         alert("Devi inserire un numero!");
         request = request + 1;
     } else {
-        userNumbers.push(userNumberControl);
+        console.log("Numero inserito: " + userNumber);
+        userNumbersSum = userNumbersSum + userNumber;
     }
 }
 
 // CHECK
-console.log(userNumbers);
-
-// SUM NUMBERS
-userNumbersSum = 0;
-
-for (var i = 0; i < userNumbers.length; i++) {
-    userNumbersSum = userNumbersSum + userNumbers[i];
-}
-
-console.log(userNumbersSum);
+console.log("Somma: " + userNumbersSum);
+// </1-fold
--- js-jsnack4/assets/main.js
+++ js-jsnack4/assets/main.js
@@ -1,18 +1,51 @@
+// <1-fold --- VERS 1 --- err: i modified
+// // ASSEGNAZIONE VARIABILI
+// // array with user's numbers
+// var userNumbers = [];
+// // control var --> if isNaN restart the cicle!
+// var userNumberControl;
+//
+// // ASK FOR A NUMBER
+// for (var i = 0; i < 6; i++) {
+//     // i iteration - insert a number with prompt
+//     userNumberControl = parseInt(prompt("Inserisci un numero"));
+//
+//     if (isNaN(userNumberControl)) {
+//         // isNaN - repeat the cicle
+//         alert("Devi inserire un numero!");
+//         i = i - 1;
+//     } else if (userNumberControl % 2 == 0) {
+//         // userNumber even
+//         console.log("Il numero inserito è pari: " + userNumberControl);
+//     } else {
+//         // userNumber odd
+//         userNumbers.push(userNumberControl);
+//         console.log("Il numero inserito è dispari: " + userNumberControl);
+//     }
+// }
+//
+// // CHECK
+// console.log(userNumbers);
+// </1-fold
+
+// <1-fold --- VERS 2 --- corrected error
 // ASSEGNAZIONE VARIABILI
 // array with user's numbers
 var userNumbers = [];
 // control var --> if isNaN restart the cicle!
 var userNumberControl;
+// request var
+var request = 6;
 
 // ASK FOR A NUMBER
-for (var i = 0; i < 6; i++) {
+for (var i = 0; i < request; i++) {
     // i iteration - insert a number with prompt
     userNumberControl = parseInt(prompt("Inserisci un numero"));
 
     if (isNaN(userNumberControl)) {
         // isNaN - repeat the cicle
         alert("Devi inserire un numero!");
-        i = i - 1;
+        request = request + 1;
     } else if (userNumberControl % 2 == 0) {
         // userNumber even
         console.log("Il numero inserito è pari: " + userNumberControl);
@@ -25,3 +58,4 @@ for (var i = 0; i < 6; i++) {
 
 // CHECK
 console.log(userNumbers);
+// </1-fold
