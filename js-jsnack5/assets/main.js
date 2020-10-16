ca38fadcd63d7f8da8432336a63c2a7d668a02c9
Marco Guidi <mark.88g@gmail.com>
2020-10-16 13:30:27 +0200

    corretti js 5 e 6

--- js-jsnack5/assets/main.js
+++ js-jsnack5/assets/main.js
@@ -1,8 +1,35 @@
+// <1-fold --- VERS 1.0 --- whit array
+// // ASSEGNAZIONE VARIABILI
+// // user's natural number
+// var number;
+// // first n-cube
+// var firstNCube = [];
+//
+// // ASK FOR A NUMBER
+// number = parseInt(prompt("Inserisci un numero naturale positivo:"));
+//
+// console.log("Numero scelto: " + number);
+//
+//
+// // CHECK THE NUMBER AND STAMP THE FIRST N CUBE
+// if (!isNaN(number) && number >= 1) {
+//     for (var i = 0; i <= number; i++) {
+//         // CREATION ARREY - FIRST N NUMBER
+//         firstNCube.push(i * i * i)
+//         console.log(i + " al cubo è " + i * i * i);
+//     }
+// } else {
+//     // is Not a Number or is a negative number
+//     alert("Numero non corretto. Refresha e inserisci un nuovo numero.")
+// }
+//
+// console.log(firstNCube);
+// </1-fold
+
+// <1-fold --- VERS 2.0 --- without array
 // ASSEGNAZIONE VARIABILI
 // user's natural number
 var number;
-// first n-cube
-var firstNCube = [];
 
 // ASK FOR A NUMBER
 number = parseInt(prompt("Inserisci un numero naturale positivo:"));
@@ -12,14 +39,12 @@ console.log("Numero scelto: " + number);
 
 // CHECK THE NUMBER AND STAMP THE FIRST N CUBE
 if (!isNaN(number) && number >= 1) {
-    for (var i = 1; i <= number; i++) {
+    for (var i = 0; i <= number; i++) {
         // CREATION ARREY - FIRST N NUMBER
-        firstNCube.push(i * i * i)
         console.log(i + " al cubo è " + i * i * i);
     }
 } else {
     // is Not a Number or is a negative number
     alert("Numero non corretto. Refresha e inserisci un nuovo numero.")
 }
-
-console.log(firstNCube);
+// </1-fold
--- js-jsnack6/assets/main.js
+++ js-jsnack6/assets/main.js
@@ -1,14 +1,29 @@
+// <1-fold --- VERSION 1.0 --- with array
+// // POWER OF 2
+// var powerOf2 = [];
+// // control var
+// var powerOf2StepI = 0;
+//
+// for (var i = 0; powerOf2StepI < 1000; i++) {
+//     powerOf2StepI = Math.pow(2, i);
+//     if (powerOf2StepI < 1000) {
+//         powerOf2.push(powerOf2StepI);
+//         console.log("2 alla " + i + " è uguale a " + powerOf2StepI);
+//     }
+// }
+//
+// console.log(powerOf2);
+// </1-fold
+
+// <1-fold --- VERSION 2.0 --- without array
 // POWER OF 2
-var powerOf2 = [];
 // control var
 var powerOf2StepI = 0;
 
 for (var i = 0; powerOf2StepI < 1000; i++) {
     powerOf2StepI = Math.pow(2, i);
     if (powerOf2StepI < 1000) {
-        powerOf2.push(powerOf2StepI);
         console.log("2 alla " + i + " è uguale a " + powerOf2StepI);
     }
 }
-
-console.log(powerOf2);
+// </1-fold
